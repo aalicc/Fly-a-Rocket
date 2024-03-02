a48a9b3f0a6ee25e9f9934d2ae77d12a3cb58b1e
Alissa Subina <alissa.subina@gmail.com>
2024-03-02 19:36:37 +0200

    try second push

--- Rocket_trajectory_FaRc4_handout.py
+++ Rocket_trajectory_FaRc4_handout.py
@@ -14,6 +14,7 @@ import matplotlib.pyplot as plt # plotting
 
 
 # ========================= Constants & parameters ========================== #
+#chmoki
 
 # Constants
 g = 9.81				# gravitational acceleration [m/s^2]
